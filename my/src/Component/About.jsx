ff1f004e176d21c431279f481186e069d1bb3da3
maaz <mmaaz7460@gmail.com>
2023-05-02 20:10:29 +0530

    update

--- my/src/Component/About.jsx
+++ my/src/Component/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
         <Heading mt = '250px' color = '#1A202C' fontSize={35}><Text as = 'span' color = '#1A202C'>About</Text> Me</Heading>
 
 
-        <Text mt = '50px' fontSize={'20'}color = '#1A202C' fontWeight={500}  >Hello 👋 Myself Maaz</Text>
+        <Text  mt = '50px' fontSize={'20'}color = '#1A202C' fontWeight={500}  >Hello 👋 Myself Maaz</Text>
 
         <Box className = {style.aboutme} border="2px solid rgb(128, 203, 196)"  color = 'black' p = {{base : '8','486px' : '10'}} borderRadius={'10px'} bg = 'rgb(224, 242, 241)' w = {{base : '100%','410px' : '300px','486px':'400px'}} m = 'auto' mt = '20px'>
 
